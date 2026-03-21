d9ca15690bd4b4c43585fa64d7414fb5f0b32f44
Garaii <ikram.gara@mail.mcgill.ca>
2026-03-21 12:06:31 -0400

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzZtjU9B4E87viNfWBIvgu8FbJ7UrciuA",
+  authDomain: "scale-of-the-univrse.firebaseapp.com",
+  projectId: "scale-of-the-univrse",
+  storageBucket: "scale-of-the-univrse.firebasestorage.app",
+  messagingSenderId: "447028819659",
+  appId: "1:447028819659:web:c66a9202076698b26c4d4e",
+  measurementId: "G-R6T68SJ22C"
+};
+
+const app = initializeApp(firebaseConfig);
